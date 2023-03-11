2c7e8c1c5c9f95a5cc5ed89b266a48956dd9cbde
devnos <iamnoelsto@gmail.com>
2023-03-11 15:46:20 +0100

    Refactor button clicks to a single object

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,23 +9,37 @@ const Button = ({onClick, text}) => {
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  // save click count of feedback buttons into a single object
+  const [clicks, setClicks] = useState({good:0, neutral:0, bad:0})
+
+  const handleGood = () => {
+    setClicks({...clicks, good: clicks.good + 1})
+  }
+  const handleNeutral = () => {
+    setClicks({...clicks, neutral: clicks.neutral + 1})
+  }
+  const handleBad = () => {
+    setClicks({...clicks, bad: clicks.bad + 1})
+  }
+
+  const totalClicks = clicks.good + clicks.neutral + clicks.bad
+  const averageClicks = totalClicks > 0 ? ((clicks.good * 1) + (clicks.neutral * 0 ) + (clicks.bad * -1)) / totalClicks : 0
+  const positiveClicks = totalClicks > 0 ? (clicks.good / totalClicks * 100) : 0
 
   return (
     <div>
       <h2>give feedback</h2>
-      <Button onClick={() => setGood(good + 1)} text='good'></Button>
-      <Button onClick={() => setNeutral(neutral + 1)} text='neutral'></Button>
-      <Button onClick={() => setBad(bad + 1)} text='bad'></Button>
+      <Button onClick={() => handleGood()} text='good'></Button>
+      <Button onClick={() => handleNeutral()} text='neutral'></Button>
+      <Button onClick={() => handleBad()} text='bad'></Button>
 
       <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>Total feedback given: {good+neutral+bad}</p>
+      <p>Good {clicks.good}</p>
+      <p>Neutral {clicks.neutral}</p>
+      <p>Bad {clicks.bad}</p>
+      <p>Total feedback given: {totalClicks}</p>
+      <p>Average feedback given: {averageClicks}</p>
+      <p>Positive feedback given: {positiveClicks}%</p>
     </div>
   )
 }
