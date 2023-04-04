dc1ab97b8a0b20f668447f6031897eb64efc19b1
Noel Tomas <iamnoelsto@gmail.com>
2023-04-04 19:06:06 +0200

    Introduce DisplayNumbers which optionally filters which numbers to display

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,36 @@
 import { useState } from "react";
 
+const DisplayNumbers = ({ persons, filteredPersons }) => {
+  if (filteredPersons.length > 0) {
+    return filteredPersons.map((person) => (
+      <p key={person.id}>
+        {person.name} {person.number}
+      </p>
+    ));
+  } else {
+    return persons.map((person) => (
+      <p key={person.id}>
+        {person.name} {person.number}
+      </p>
+    ));
+  }
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: "1", name: "Arto Hellas", number: "31684327128" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("la");
   const nameExists = persons.some((person) => person.name === newName);
   const numberExists = persons.some((person) => person.number === newNumber);
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(newFilter.toLowerCase())
+  );
 
   const addNewEntry = (event) => {
     event.preventDefault();
@@ -33,6 +56,9 @@ const App = () => {
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleNewFilter = (event) => {
+    setNewFilter(event.target.value);
+  };
 
   return (
     <div>
@@ -49,11 +75,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.id}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      <p>
+        Filter: <input value={newFilter} onChange={handleNewFilter} />
+      </p>
+      <DisplayNumbers persons={persons} filteredPersons={filteredPersons} />
     </div>
   );
 };
