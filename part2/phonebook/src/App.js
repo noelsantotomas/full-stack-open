ff8daefe175aaac90b78c2f2ce5ca0e02e1e0ee2
Noel Tomas <iamnoelsto@gmail.com>
2023-04-04 20:07:17 +0200

    Extract components from App

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,7 @@
 import { useState } from "react";
-
-const DisplayNumbers = ({ persons, filteredPersons }) => {
-  if (filteredPersons.length > 0) {
-    return filteredPersons.map((person) => (
-      <p key={person.id}>
-        {person.name} {person.number}
-      </p>
-    ));
-  } else {
-    return persons.map((person) => (
-      <p key={person.id}>
-        {person.name} {person.number}
-      </p>
-    ));
-  }
-};
+import DisplayNumbers from "./components/DisplayNumbers";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -25,59 +12,25 @@ const App = () => {
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [newFilter, setNewFilter] = useState("la");
-  const nameExists = persons.some((person) => person.name === newName);
-  const numberExists = persons.some((person) => person.number === newNumber);
+  const [newFilter, setNewFilter] = useState("");
+
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(newFilter.toLowerCase())
   );
 
-  const addNewEntry = (event) => {
-    event.preventDefault();
-    const entryObject = {
-      id: persons.length + 1,
-      name: newName,
-      number: newNumber,
-    };
-    if (nameExists) {
-      alert(`${newName} is already in the phonebook.`);
-    } else if (numberExists) {
-      alert(`${newNumber} is already in the phonebook.`);
-    } else {
-      setPersons(persons.concat(entryObject));
-      setNewName("");
-      setNewNumber("");
-    }
-  };
-
-  const handleNewName = (event) => {
-    setNewName(event.target.value);
-  };
-  const handleNewNumber = (event) => {
-    setNewNumber(event.target.value);
-  };
-  const handleNewFilter = (event) => {
-    setNewFilter(event.target.value);
-  };
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNewEntry}>
-        <div>
-          Name: <input value={newName} onChange={handleNewName} />
-        </div>
-        <div>
-          Number: <input value={newNumber} onChange={handleNewNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
       <h2>Numbers</h2>
-      <p>
-        Filter: <input value={newFilter} onChange={handleNewFilter} />
-      </p>
+      <Filter newFilter={newFilter} setNewFilter={setNewFilter} />
       <DisplayNumbers persons={persons} filteredPersons={filteredPersons} />
     </div>
   );
--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,10 +1,39 @@
 const PersonForm = ({
-  addNewEntry,
+  persons,
+  setPersons,
   newName,
-  handleNewName,
+  setNewName,
   newNumber,
-  handleNewNumber,
+  setNewNumber,
 }) => {
+  const nameExists = persons.some((person) => person.name === newName);
+  const numberExists = persons.some((person) => person.number === newNumber);
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const addNewEntry = (event) => {
+    event.preventDefault();
+    const entryObject = {
+      id: persons.length + 1,
+      name: newName,
+      number: newNumber,
+    };
+    if (nameExists) {
+      alert(`${newName} is already in the phonebook.`);
+    } else if (numberExists) {
+      alert(`${newNumber} is already in the phonebook.`);
+    } else {
+      setPersons(persons.concat(entryObject));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
   return (
     <form onSubmit={addNewEntry}>
       <div>
