000304ebe1f188bccc03018886c91a148744eb85
Noel Tomas <iamnoelsto@gmail.com>
2023-08-02 10:48:14 +0200

    Add logic to display a country's info if only one country matches the input value

--- part2/countries/src/DisplayCountries.js
+++ part2/countries/src/DisplayCountries.js
@@ -1,10 +1,28 @@
 const DisplayCountries = ({ countriesToShow }) => {
-  if (countriesToShow.length < 10) {
-    return countriesToShow.map((country) => (
-      <div key={country.id}>{country.name.common} </div>
+  if (countriesToShow.length === 1) {
+    return countriesToShow.map((country, index) => (
+      <div key={index}>
+        <h1>{country.name.common}</h1>
+        <p>Capital: {country.capital}</p>
+        <p>Region: {country.region}</p>
+        <h2>Languages</h2>
+        {Object.values(country.languages).map((language, index) => (
+          <ul key={index}>
+            <li>{language}</li>
+          </ul>
+        ))}
+        <h2>Flag</h2>
+        <img src={country.flags.png} alt={country.flags.alt}></img>
+      </div>
     ));
-  } else if (countriesToShow.length > 10) {
-    return <p>Too many results</p>;
+  } else if (countriesToShow.length < 10) {
+    return countriesToShow.map((country, index) => (
+      <div key={index}>{country.name.common} </div>
+    ));
+  } else {
+    return (
+      <p>Too many results, narrow down search by adding more characters.</p>
+    );
   }
 };
 
